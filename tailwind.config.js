5732209a46966acde7f9c6764b20e823f84e750a
Jerry Mbola <martinmbola@gmail.com>
2023-08-15 00:28:34 +0300

    addew new html path for tailwind_components practice

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html"],
+  //content: ['./public/index.html', './day_29_index.html'] - two HTML paths in the public folder
+  //content: ["./public/index.html"] - initial path (only one HTML file in public folder)
+  content: ['./public/**/*.html'],
   theme: {
     extend: {},
   },
